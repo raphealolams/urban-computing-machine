1b6c0415354e9ccf6650cb0e507ff79be4f2f778
raphealolams <raphealolams@gmail.com>
2019-03-04 18:29:52 +0100

    && came

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const writer = require('./lib/writer')
 
-const path = './file/file2.xlsx'
+const path = './file/file3.xlsx'
 const fileName = "test.json"
 
-
-
 function call(path) {
+
     return writer.writeJson(path, fileName)
 }
 
 call(path)
+
